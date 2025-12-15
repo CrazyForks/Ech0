df6c58e2e911a645b002d27bb02cd7cca093ab33
L1nSn0w <l1nsn0w@qq.com>
2025-12-15 22:21:44 +0800

    Revert "feat: 优化图片按钮样式，支持自适应高度和宽度"
    This reverts commit 8bd38e83ca28af914a3f8bd101fd22cb831885ea.

--- web/src/components/advanced/TheImageGallery.vue
+++ web/src/components/advanced/TheImageGallery.vue
@@ -11,18 +11,15 @@
       <button
         v-for="(src, idx) in images"
         :key="idx"
-        class="bg-transparent border-0 p-0 cursor-pointer relative block w-full overflow-hidden"
+        class="bg-transparent border-0 p-0 cursor-pointer w-fit"
         :class="getColSpan(idx, images.length)"
-        :style="{
-          paddingTop: src.width && src.height ? `${(src.height / src.width) * 100}%` : '100%',
-        }"
         @click="openFancybox(idx)"
       >
         <img
           :src="baseUrl ? getHubImageUrl(src, baseUrl) : getImageUrl(src)"
           :alt="`预览图片${idx + 1}`"
           loading="lazy"
-          class="echoimg w-full h-full object-cover absolute top-0 left-0",
+          class="echoimg block max-w-full h-auto"
         />
       </button>
     </div>
